15c86c95921145ae66494684c4c4327afb4dd4b3
Stefan <sb223cd@student.lnu.se>
2017-09-29 05:43:36 +0200

    Getters and Setters - Member,Boat

--- src/model/Member.java
+++ src/model/Member.java
@@ -9,10 +9,33 @@ package model;
  */
 public class Member {
 	
-	private String name;
-	
 	private int persNum;
-	private int identity;
-	private int nrOfBoats;
+	private int id;
+	private int numOfBoats;
 
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPersNum() {
+		return persNum;
+	}
+	public void setPersNum(int persNum) {
+		this.persNum = persNum;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getNumOfBoats() {
+		return numOfBoats;
+	}
+	public void setNumOfBoats(int numOfBoats) {
+		this.numOfBoats = numOfBoats;
+	}
 }
